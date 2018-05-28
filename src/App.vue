7ca8be8ffc415004d7dcfeaea0ef71e32044a08f
Lei Xu <komushi@gmail.com>
2018-05-28 15:44:26 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <apollo-example></apollo-example>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ApolloExample from './components/ApolloExample.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    ApolloExample
   }
 }
 </script>
